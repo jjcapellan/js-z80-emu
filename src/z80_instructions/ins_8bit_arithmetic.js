33dde34f81c9b3668e4316e4619b29c62d6169c8
jjcapellan <soycape@hotmail.com>
2021-06-29 18:43:54 +0200

    Instructions: add A, (IX + d)

--- src/z80_instructions/ins_8bit_arithmetic.js
+++ src/z80_instructions/ins_8bit_arithmetic.js
@@ -5,16 +5,16 @@
  * @author Juan Jose Capellan <soycape@hotmail.com>
  */
 
- const { regsSp } = require("../z80_registers");
+const { regsSp } = require("../z80_registers");
 
- /**
- * ADD A, r
- * 
- * The contents of register r are added to the contents of the Accumulator, and the result is
- * stored in the Accumulator. The r symbol identifies the registers A, B, C, D, E, H, or L,
- * Clock: 4T
- */
-function add_A_r(cpu, rIndex){
+/**
+* ADD A, r
+* 
+* The contents of register r are added to the contents of the Accumulator, and the result is
+* stored in the Accumulator. The r symbol identifies the registers A, B, C, D, E, H, or L,
+* Clock: 4T
+*/
+function add_A_r(cpu, rIndex) {
     const regs = cpu.registers.regs8;
     const r = regs.get(rIndex);
     const a = regs.get(regs.idx.A);
@@ -29,14 +29,14 @@ function add_A_r(cpu, rIndex){
     flags.set(flags.idx.C, (result & 0b100000000) == 1);
 }
 
- /**
- * ADD A, n
- * 
- * The n integer is added to the contents of the Accumulator, and the results are stored in the
- * Accumulator.
- * Clock: 7T
- */
-function add_A_n(cpu, n){
+/**
+* ADD A, n
+* 
+* The n integer is added to the contents of the Accumulator, and the results are stored in the
+* Accumulator.
+* Clock: 7T
+*/
+function add_A_n(cpu, n) {
     const regs = cpu.registers.regs8;
     const a = regs.get(regs.idx.A);
     regs.set(regs.idx.A, a + n);
@@ -50,14 +50,14 @@ function add_A_n(cpu, n){
     flags.set(flags.idx.C, (result & 0b100000000) == 1);
 }
 
- /**
- * ADD A, (HL)
- * 
- * The byte at the memory address specified by the contents of the HL register pair is added
- * to the contents of the Accumulator, and the result is stored in the Accumulator.
- * Clock: 7T
- */
-  function add_A_ptrHL(cpu){
+/**
+* ADD A, (HL)
+* 
+* The byte at the memory address specified by the contents of the HL register pair is added
+* to the contents of the Accumulator, and the result is stored in the Accumulator.
+* Clock: 7T
+*/
+function add_A_ptrHL(cpu) {
     const regs = cpu.registers.regs8;
     const regs16 = cpu.registers.regs16;
     const a = regs.get(regs.idx.A);
@@ -74,6 +74,60 @@ function add_A_n(cpu, n){
     flags.set(flags.idx.C, (result & 0b100000000) == 1);
 }
 
+/**
+* ADD A, (IX + d)
+* 
+* The contents of the Index (register pair IX) Register is added to a two’s 
+* complement displacement d to point to an address in memory. The contents of this address is then added to
+* the contents of the Accumulator and the result is stored in the Accumulator.
+* Clock: 19T
+*/
+function add_A_ptrIXplusd(cpu, d) {
+    const regs = cpu.registers.regs8;
+    const regsSp = cpu.registers.regsSp;
+    const a = regs.get(regs.idx.A);
+    const ix = regsSp.IX;
+    const n = ix + d;
+    regs.set(regs.idx.A, a + n);
+    // Flags
+    const result = a + n;
+    flags.set(flags.idx.S, (result & 0b10000000) != 0);
+    flags.set(flags.idx.Z, result == 0);
+    flags.set(flags.idx.H, ((n & 0b1000) + (a & 0b1000)) == 0b10);
+    flags.set(flags.idx.PV, (result & 0b100000000) == 1);
+    flags.set(flags.idx.N, false);
+    flags.set(flags.idx.C, (result & 0b100000000) == 1);
+}
+
+/**
+* ADD A, (IY + d)
+* 
+* The contents of the Index (register pair IY) Register is added to a two’s 
+* complement displacement d to point to an address in memory. The contents of this address is then added to
+* the contents of the Accumulator, and the result is stored in the Accumulator.
+* Clock: 19T
+*/
+function add_A_ptrIYplusd(cpu, d) {
+    const regs = cpu.registers.regs8;
+    const regsSp = cpu.registers.regsSp;
+    const a = regs.get(regs.idx.A);
+    const iy = regsSp.IY;
+    const n = ix + d;
+    regs.set(regs.idx.A, a + n);
+    // Flags
+    const result = a + n;
+    flags.set(flags.idx.S, (result & 0b10000000) != 0);
+    flags.set(flags.idx.Z, result == 0);
+    flags.set(flags.idx.H, ((n & 0b1000) + (a & 0b1000)) == 0b10);
+    flags.set(flags.idx.PV, (result & 0b100000000) == 1);
+    flags.set(flags.idx.N, false);
+    flags.set(flags.idx.C, (result & 0b100000000) == 1);
+}
+
 module.exports = {
-    add_A_r
+    add_A_r,
+    add_A_n,
+    add_A_ptrHL,
+    add_A_ptrIXplusd,
+    add_A_ptrIYplusd
 }
