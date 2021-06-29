4276445629c8fb314e8f21e0e7033b6272078eec
jjcapellan <soycape@hotmail.com>
2021-06-29 20:14:14 +0200

    Instruction: all sub instructions

--- src/z80_instructions/ins_8bit_arithmetic.js
+++ src/z80_instructions/ins_8bit_arithmetic.js
@@ -18,6 +18,17 @@ function setAddFlags(cpu, s1, s2) {
     flags.set(flags.idx.C, (sum & 0x100) == 1);
 }
 
+function setSubsFlags(cpu, s1, s2) {
+    const flags = cpu.registers.flags;
+    const sum = s1 - s2;
+    flags.set(flags.idx.S, (sum & 0x80) != 0);
+    flags.set(flags.idx.Z, sum == 0);
+    flags.set(flags.idx.H, ((s1 & 0x8) + (s2 & 0x8)) == 0b10);
+    flags.set(flags.idx.PV, (sum & 0x100) == 1);
+    flags.set(flags.idx.N, false);
+    flags.set(flags.idx.C, (sum & 0x100) == 1);
+}
+
 /**
 * ADD A, r
 * 
@@ -198,6 +209,88 @@ function adc_A_ptrIYplusd(cpu, d) {
     setAddFlags(cpu, a, n);
 }
 
+/**
+* SUB A, r
+* 
+* The contents of register r are substracted to the contents of the Accumulator, and the result is
+* stored in the Accumulator. The r symbol identifies the registers A, B, C, D, E, H, or L,
+* Clock: 4T
+*/
+function sub_A_r(cpu, rIndex) {
+    const regs = cpu.registers.regs8;
+    const r = regs.get(rIndex);
+    const a = regs.get(regs.idx.A);
+    regs.set(regs.idx.A, a - r);
+    setSubsFlags(cpu, a, r);
+}
+
+/**
+* SUB A, n
+* 
+* The n integer is substracted to the contents of the Accumulator, and the results are stored in the
+* Accumulator.
+* Clock: 7T
+*/
+function sub_A_n(cpu, n) {
+    const regs = cpu.registers.regs8;
+    const a = regs.get(regs.idx.A);
+    regs.set(regs.idx.A, a - n);
+    setSubsFlags(cpu, a, n);
+}
+
+/**
+* SUB A, (HL)
+* 
+* The byte at the memory address specified by the contents of the HL register pair is substracted
+* to the contents of the Accumulator, and the result is stored in the Accumulator.
+* Clock: 7T
+*/
+function sub_A_ptrHL(cpu) {
+    const regs = cpu.registers.regs8;
+    const regs16 = cpu.registers.regs16;
+    const a = regs.get(regs.idx.A);
+    const hl = regs16.get(regs16.idx.HL);
+    const n = cpu.memory[hl];
+    regs.set(regs.idx.A, a - n);
+    setSubsFlags(cpu, a, n);
+}
+
+/**
+* SUB A, (IX + d)
+* 
+* The contents of the Index (register pair IX) Register is added to a two’s 
+* complement displacement d to point to an address in memory. The contents of this address is then substracted 
+* to the contents of the Accumulator and the result is stored in the Accumulator.
+* Clock: 19T
+*/
+function sub_A_ptrIXplusd(cpu, d) {
+    const regs = cpu.registers.regs8;
+    const regsSp = cpu.registers.regsSp;
+    const a = regs.get(regs.idx.A);
+    const ix = regsSp.IX;
+    const n = cpu.memory[ix + d];
+    regs.set(regs.idx.A, a - n);
+    setSubsFlags(cpu, a, n);
+}
+
+/**
+* SUB A, (IY + d)
+* 
+* The contents of the Index (register pair IY) Register is added to a two’s 
+* complement displacement d to point to an address in memory. The contents of this address is then substracted
+* to the contents of the Accumulator, and the result is stored in the Accumulator.
+* Clock: 19T
+*/
+function sub_A_ptrIYplusd(cpu, d) {
+    const regs = cpu.registers.regs8;
+    const regsSp = cpu.registers.regsSp;
+    const a = regs.get(regs.idx.A);
+    const iy = regsSp.IY;
+    const n = cpu.memory[iy + d];
+    regs.set(regs.idx.A, a - n);
+    setSubsFlags(cpu, a, n);
+}
+
 module.exports = {
     add_A_r,
     add_A_n,
@@ -208,5 +301,10 @@ module.exports = {
     adc_A_n,
     adc_A_ptrHL,
     adc_A_ptrIXplusd,
-    adc_A_ptrIYplusd
+    adc_A_ptrIYplusd,
+    sub_A_r,
+    sub_A_n,
+    sub_A_ptrHL,
+    sub_A_ptrIXplusd,
+    sub_A_ptrIYplusd
 }
