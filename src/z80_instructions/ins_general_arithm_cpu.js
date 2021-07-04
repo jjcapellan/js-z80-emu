049e4f55bf08947ab0f1f313f5df58852d04233e
jjcapellan <soycape@hotmail.com>
2021-07-04 21:16:24 +0200

    Instruction: daa

---
+++ src/z80_instructions/ins_general_arithm_cpu.js
@@ -0,0 +1,54 @@
+/**
+ * This file implements Z80 general purpose arithmetic and cpu control instructions group
+ * Info on page 172 of [manual](http://www.zilog.com/docs/z80/um0080.pdf)
+ * 
+ * @author Juan Jose Capellan <soycape@hotmail.com>
+ */
+
+//Flag masks
+const S = 0b10000000;
+const Z = 0b01000000;
+const F5 = 0b00100000;
+const H = 0b00010000;
+const F3 = 0b00001000;
+const PV = 0b00000100;
+const N = 0b00000010;
+const C = 0b00000001;
+
+/**
+* DAA
+* 
+* This instruction conditionally adjusts the Accumulator for BCD addition and subtraction
+* operations. For addition (ADD, ADC, INC) or subtraction (SUB, SBC, DEC, NEG).
+* Clock: 4T
+*/
+function daa(cpu) {
+    const regs = cpu.registers.regs8;
+    const f = regs.get(regs.idx.F);
+    const a = regs.get(regs.idx.A);
+    // Get adjustment and carry flag
+    const nch = ((f & N) << 1) | ((f & C) << 1) | ((f & H) >> 4);
+    const tableData = cpu.tables.daaTable[(nch << 8) | a];
+    const adjustment = tableData & 0x00ff;
+    const carryFlag = tableData >> 8;
+    // Calc of flags
+    const result = a + adjustment;
+    let flags = 0;
+    flags |= carryFlag;
+    if (result & 0x80) flags |= S;
+    if ((result & 0xff) == 0) flags |= Z;
+    //if ((a & 0x0f) > (result & 0x0f)) flags |= H; <-- fail in tests, try next method in other functions
+    if((a^result)&0x10) flags |= H;
+    if (cpu.tables.parityTable[(result & 0xff)]) flags |= PV;
+    flags |= (f & N);
+    if (result & F3) flags |= F3;
+    if (result & F5) flags |= F5;
+
+
+    regs.set(regs.idx.A, (result & 0xff));
+    regs.set(regs.idx.F, flags);
+}
+
+module.exports = {
+    daa
+}
---
+++ test/test_ins_general_arithm_cpu.js
@@ -0,0 +1,45 @@
+const test = require('ava');
+const z80 = require('../src/z80_cpu');
+const instr = require('../src/z80_instructions/ins_general_arithm_cpu');
+
+const cpu = new z80();
+const regs8 = cpu.registers.regs8;
+const regs16 = cpu.registers.regs16;
+const regsSp = cpu.registers.regsSp;
+const flags = cpu.registers.flags;
+const mem = cpu.memory;
+
+//Flag masks
+const S = 0b10000000;
+const Z = 0b01000000;
+const F5 = 0b00100000;
+const H = 0b00010000;
+const F3 = 0b00001000;
+const PV = 0b00000100;
+const N = 0b00000010;
+const C = 0b00000001;
+
+test('daa(cpu)', t => {
+
+    function doTest(a0, f0, a1, f1){
+    regs8.set(regs8.idx.A, a0);
+    regs8.set(regs8.idx.F, f0);
+    instr.daa(cpu); // DAA
+    a = regs8.get(regs8.idx.A);
+    f = regs8.get(regs8.idx.F);    
+    t.is(a, a1, `a0: ${a0.toString(16)}, a1: ${a1.toString(16)}, result_a:${a.toString(16)}`);
+    t.is(f, f1, `a0: ${a0.toString(16)}, f1:${f1.toString(16)}, result_f:${f.toString(16)}`);
+    }
+
+    doTest(0x2c, 0, 0x2c + 0x6, 0x30);
+    doTest(0xc6, 0, (0xc6 + 0x60) & 0xff, 0x21);
+    doTest(0x62, H, 0x62 + 0x6, 0x28);
+    doTest(0x23, C, 0x23 + 0x60, 0x81);
+    doTest(0x2b, C, (0x2b + 0x66) & 0xff, 0x91);
+    doTest(0x23, C | H, 0x23 + 0x66, 0x89);
+    doTest(0x23, C | H, 0x23 + 0x66, 0x89);
+    doTest(0x23, N, 0x23, 0x22);
+    doTest(0x28, N | H, (0x28 + 0xfa) & 0xff, 0x26);
+    doTest(0x88, C | H | N, (0x88 + 0x9a) & 0xff, 0x27);
+    
+});
