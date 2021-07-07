09bd52978585544943c43cde9c9f957d4dae6521
jjcapellan <soycape@hotmail.com>
2021-07-07 11:19:33 +0200

    Added: add_HL_ss and adc_HL_ss test

--- test/test_ins_16bit_arithmetic.js
+++ test/test_ins_16bit_arithmetic.js
@@ -12,22 +12,34 @@ const fi = flags.idx;
 const mem = cpu.memory;
 
 test('add_HL_ss(ssIndex)', t => {
-    let ssIndex = i16.BC;
-    r16.set(ssIndex, 0x2324);
-    r16.set(i16.HL, 0x1);
-    
-    instr.add_HL_ss(ssIndex); // ADD HL, ss
-    let hl = r16.get(i16.HL);
-    let f = r8.get(i8.F);
-    t.is(hl, 0x2325);
-    t.is(f, 0x20);
+    function doTest(hl0, bcValue, f0, hl1, f1) {
+        let ssIndex = i16.BC;
+        r16.set(ssIndex, bcValue);
+        r16.set(i16.HL, hl0);
+        instr.add_HL_ss(ssIndex); // ADD HL, ss
+        let hl = r16.get(i16.HL);
+        let f = r8.get(i8.F);
+        t.is(hl, hl1);
+        t.is(f, f1);
+    }
 
-    ssIndex = i16.BC;
-    r16.set(ssIndex, 0x6f24);
-    r16.set(i16.HL, 0x1f32);
-    instr.add_HL_ss(ssIndex); // ADD HL, ss
-    hl = r16.get(i16.HL);
-    f = r8.get(i8.F);
-    t.is(hl, 0x8e56);
-    t.is(f, 0x18);
+    doTest(0x1, 0x2324, 0, 0x2325, 0x20);
+    doTest(0x1f32, 0x6f24, 0, 0x8e56, 0x18);
+});
+
+test('adc_HL_ss(ssIndex)', t => {
+    function doTest(hl0, bcValue, f0, hl1, f1) {
+        let ssIndex = i16.BC;
+        r16.set(ssIndex, bcValue);
+        r16.set(i16.HL, hl0);
+        r8.set(i8.F, f0);
+        instr.adc_HL_ss(ssIndex); // ADD HL, ss
+        let hl = r16.get(i16.HL);
+        let f = r8.get(i8.F);
+        t.is(hl, hl1);
+        t.is(f, f1);
+    }
+
+    doTest(0x1, 0x2324, 0x1, 0x2326, 0x20);
+    doTest(0x1f32, 0x6f24, 0, 0x8e56, 0x9c);
 });
---
+++ test/test_ins_rotate_shift.js
@@ -0,0 +1,26 @@
+const test = require('ava');
+const z80 = require('../src/z80_cpu');
+const instr = require('../src/z80_instructions/ins_rotate_shift');
+
+const cpu = new z80();
+const r8 = cpu.registers.regs8;
+const i8 = r8.idx;
+const r16 = cpu.registers.regs16;
+const i16 = r16.idx;
+const flags = cpu.registers.flags;
+const fi = flags.idx;
+const mem = cpu.memory;
+
+test('rlca', t => {
+    function doTest(a0, a1, f0, f1) {
+        r8.set(i8.A, a0);
+        r8.set(i8.F, f0);
+        instr.rlca(); // RLCA
+        let a = r8.get(i8.A);
+        let f = r8.get(i8.F);
+        t.is(a, a1);
+        t.is(f, f1);
+    }
+
+    doTest(0x24, 0x48, 0x5d, 0x4c);
+});
