d4d4428a8fd35afaee07460e8a2fc7602251141b
jjcapellan <soycape@hotmail.com>
2021-07-07 11:31:24 +0200

    Instruction: rla

--- src/z80_instructions/ins_rotate_shift.js
+++ src/z80_instructions/ins_rotate_shift.js
@@ -29,7 +29,28 @@ function rlca() {
     flags.set(fi.F5, (aRotated & (1 << fi.F5)) != 0);
 }
 
+/**
+* RLA
+* 
+* The contents of the Accumulator (Register A) are rotated left 1 bit position through the
+* Carry flag. The previous contents of the Carry flag are copied to bit 0. Bit 0 is the leastsignificant bit.
+* Clock: 4T
+*/
+function rla() {
+    const a = r8.get(i8.A);
+    const bit7 = a >> 7;
+    const cf = flags.get(fi.C);
+    aRotated = ((a << 1) & 0xff) | cf;
+    r8.set(i8.A, aRotated);
+    flags.set(fi.C, bit7);
+    flags.set(fi.N, false);
+    flags.set(fi.H, false);
+    flags.set(fi.F3, (aRotated & (1 << fi.F3)) != 0);
+    flags.set(fi.F5, (aRotated & (1 << fi.F5)) != 0);
+}
+
 module.exports = {
     rlca,
+    rla,
     setCPU
 }
