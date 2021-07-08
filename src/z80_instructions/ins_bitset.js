78621793379f56c014344e673ad43b05cb248355
jjcapellan <soycape@hotmail.com>
2021-07-09 00:31:58 +0200

    Instruction: BIT b, r

---
+++ src/z80_instructions/ins_bitset.js
@@ -0,0 +1,27 @@
+/**
+ * This file implements Z80 bit set, reset, and test instructions group
+ * Info on page 242 of [manual](http://www.zilog.com/docs/z80/um0080.pdf)
+ * 
+ * @author Juan Jose Capellan <soycape@hotmail.com>
+ */
+
+let CPU, r8, i8, r16, i16, flags, fi, mem;
+function setCPU(data) {
+    ({ CPU, r8, i8, r16, i16, flags, fi, mem } = data);
+}
+
+/**
+* BIT b, r
+* 
+* This instruction tests bit b in register r and sets the Z flag accordingly.
+* Clock: 8T
+*/
+function bit_b_r(b, rIndex) {
+    const r = r8.get(rIndex);
+    const bit = r & (1 << b);
+    flags.set(fi.Z, bit);
+}
+
+module.exports = {
+    setCPU
+}
