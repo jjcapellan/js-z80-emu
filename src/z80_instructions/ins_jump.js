2a6445d6444183d02896e99726b7c859ed8bdad5
jjcapellan <soycape@hotmail.com>
2021-07-10 00:39:47 +0200

    Instruction: JP nn

---
+++ src/z80_instructions/ins_jump.js
@@ -0,0 +1,28 @@
+/**
+ * This file implements Z80 8bit arithmetic instructions group
+ * Info on page 261 of [manual](http://www.zilog.com/docs/z80/um0080.pdf)
+ * 
+ * @author Juan Jose Capellan <soycape@hotmail.com>
+ */
+
+let CPU, r8, i8, r16, i16, flags, fi, mem;
+function setCPU(data) {
+    ({ CPU, r8, i8, r16, i16, flags, fi, mem } = data);
+}
+
+/**
+* JP nn
+* 
+* Operand nn is loaded to register pair Program Counter (PC). The next instruction is
+* fetched from the location designated by the new contents of the PC.
+* Clock: 10T
+*/
+function jp_nn(nn) {
+    r16.set(i16.PC, nn);    
+}
+
+
+module.exports = {
+    jp_nn,
+    setCPU
+}
