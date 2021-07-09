9853da24fbe1208cb68f20b70b63b4efeb644a24
jjcapellan <soycape@hotmail.com>
2021-07-10 01:16:43 +0200

    Instruction: JR Z, e

--- src/z80_instructions/ins_jump.js
+++ src/z80_instructions/ins_jump.js
@@ -131,10 +131,48 @@ function jr_e(e) {
 * location designated by the new contents of the PC. The jump is measured from the address
 * of the instruction op code and contains a range of –126 to +129 bytes.
 * The assembler automatically adjusts for the twice incremented PC.
+* If the flag = 0, the next instruction executed is taken from the location following this
+* instruction. 
 * Clock: 12T (if condition is met)    7T (if condition is not met)
 */
 function jr_c_e(e) {
     if(flags.get(fi.C))
+    r16.set(i16.PC, r16.get(i16.PC) + e);
+}
+
+/**
+* JR NC, e
+* 
+* This instruction provides for conditional branching to other segments of a program
+* depending on the results of a test on the Carry Flag. If the flag = 0, the value of 
+* displacement e is added to the Program Counter (PC) and the next instruction is fetched from the
+* location designated by the new contents of the PC. The jump is measured from the address
+* of the instruction op code and contains a range of –126 to +129 bytes.
+* The assembler automatically adjusts for the twice incremented PC.
+* If the flag = 1, the next instruction executed is taken from the location following this
+* instruction.
+* Clock: 12T (if condition is met)    7T (if condition is not met)
+*/
+function jr_nc_e(e) {
+    if(!flags.get(fi.C))
+    r16.set(i16.PC, r16.get(i16.PC) + e);
+}
+
+/**
+* JR Z, e
+* 
+* This instruction provides for conditional branching to other segments of a program
+* depending on the results of a test on the Carry Flag. If the flag = 1, the value of 
+* displacement e is added to the Program Counter (PC) and the next instruction is fetched from the
+* location designated by the new contents of the PC. The jump is measured from the address
+* of the instruction op code and contains a range of –126 to +129 bytes.
+* The assembler automatically adjusts for the twice incremented PC.
+* If the flag = 0, the next instruction executed is taken from the location following this
+* instruction. 
+* Clock: 12T (if condition is met)    7T (if condition is not met)
+*/
+function jr_z_e(e) {
+    if(flags.get(fi.Z))
     r16.set(i16.PC, r16.get(i16.PC) + e);
 }
 
