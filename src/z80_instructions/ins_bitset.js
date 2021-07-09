234666030cb7a9f835ced5ce2868bc5e297c150f
jjcapellan <soycape@hotmail.com>
2021-07-09 19:41:34 +0200

    Instructions: BIT and SET

--- src/z80_instructions/ins_bitset.js
+++ src/z80_instructions/ins_bitset.js
@@ -62,10 +62,123 @@ function bit_b_ptrHL(b) {
     );
     r8.set(i8.F, f);
 }
-    
+
+/**
+* BIT b, (IX + d)
+* 
+* This instruction tests bit b in the memory location specified by the contents of register pair
+* IX combined with the two’s complement displacement d and sets the Z flag accordingly.
+* Clock: 20T
+*/
+function bit_b_ptrIXplusd(b, d) {
+    const ix = r16.get(i16.IX);
+    const n = mem[ix + d];
+    const z = ((n & (1 << b)) == 0) ? true : false;
+    const ixdHigh = (ix + d) >> 8;
+    let f = createFlags(
+        flags.get(fi.C),
+        false,
+        z,
+        ((ixdHigh & (1 << fi.F3)) != 0),
+        true,
+        ((ixdHigh & (1 << fi.F5)) != 0),
+        z,
+        (b == 7) && (z == 0)
+    );
+    r8.set(i8.F, f);
+}
+
+/**
+* BIT b, (IY + d)
+* 
+* This instruction tests bit b in the memory location specified by the contents of register pair
+* IY combined with the two’s complement displacement d and sets the Z flag accordingly.
+* Clock: 20T
+*/
+function bit_b_ptrIYplusd(b, d) {
+    const iy = r16.get(i16.IY);
+    const n = mem[iy + d];
+    const z = ((n & (1 << b)) == 0) ? true : false;
+    const iydHigh = (iy + d) >> 8;
+    let f = createFlags(
+        flags.get(fi.C),
+        false,
+        z,
+        ((iydHigh & (1 << fi.F3)) != 0),
+        true,
+        ((iydHigh & (1 << fi.F5)) != 0),
+        z,
+        (b == 7) && (z == 0)
+    );
+    r8.set(i8.F, f);
+}
+
+/**
+ * Helper for set instructions
+ * @param {number} b Bit 0-7
+ * @param {number} n Byte to set bit
+ * @returns Byte
+ */
+function set_n(b, n){
+    return n | (1 << b);
+}
+
+/**
+* SET b, r
+* 
+* Bit b in register r (any of registers B, C, D, E, H, L, or A) is set.
+* Clock: 8T
+*/
+function set_b_r(b, rIndex) {
+    r8.set(rIndex, set_n(b, r8.get(rIndex)));
+}
+
+/**
+* SET b, (HL)
+* 
+* Bit b in the memory location addressed by the contents of register pair HL is set.
+* Clock: 15T
+*/
+function set_b_ptrHL(b) {
+    const hl = r16.get(i16.HL);
+    let n = mem[hl];
+    mem[hl] = set_n(b, n);
+}
+
+/**
+* SET b, (IX + d)
+* 
+* Bit b in the memory location addressed by the sum of the contents of the IX register pair
+* and the two’s complement integer d is set.
+* Clock: 23T
+*/
+function set_b_ptrIXplusd(b, d) {
+    const ix = r16.get(i16.IX);
+    let n = mem[ix + d];
+    mem[ix + d] = set_n(b, n);
+}
+
+/**
+* SET b, (IY + d)
+* 
+* Bit b in the memory location addressed by the sum of the contents of the IY register pair
+* and the two’s complement integer d is set.
+* Clock: 23T
+*/
+function set_b_ptrIYplusd(b, d) {
+    const iy = r16.get(i16.IY);
+    let n = mem[iy + d];
+    mem[iy + d] = set_n(b, n);
+}
     
 module.exports = {
     bit_b_r,
     bit_b_ptrHL,
+    bit_b_ptrIXplusd,
+    bit_b_ptrIYplusd,
+    set_b_r,
+    set_b_ptrHL,
+    set_b_ptrIXplusd,
+    set_b_ptrIYplusd,
     setCPU
 }
--- test/test_ins_bitset.js
+++ test/test_ins_bitset.js
@@ -24,6 +24,52 @@ test('bit_b_r(b, rIndex)', t => {
         t.is(f, f1, `Result: ${f.toString(16)} Expected: ${f1.toString(16)}`);
     }
 
-    doTest(7, 0xf4, 0xf4, 0x5c, 0xb0);
+    doTest(7, 0xf4, 0xf4, 0x5c, 0x90); // <-- differs from retro virtual machine (it activates F5 flag)
+    doTest(4, 0x41, 0x41, 0x01, 0x55); 
+});
+
+test('bit_b_ptrHL(b)', t => {
+    function doTest(b, n0, n1, f0, f1) {
+        r16.set(i16.HL, 0x5c9d);
+        const hl = r16.get(i16.HL);
+        mem[hl] = n0;
+        r8.set(i8.F, f0);
+        instr.bit_b_ptrHL(b); // BIT b, (hl)
+        let n = mem[hl];
+        let f = r8.get(i8.F);
+        t.is(n, n1);
+        t.is(f, f1, `Result: ${f.toString(16)} Expected: ${f1.toString(16)}`);
+    }
+
+    doTest(7, 0xf4, 0xf4, 0x00, 0x90);
     doTest(4, 0x41, 0x41, 0x01, 0x55);
+});
+
+test('bit_b_ptrIXplusd(b, d)', t => {
+    function doTest(b, d, n0, n1, f0, f1) {
+        r16.set(i16.IX, 0x5c9c);
+        const ix = r16.get(i16.IX);
+        mem[ix+d] = n0;
+        r8.set(i8.F, f0);
+        instr.bit_b_ptrIXplusd(b, d); // BIT b, (IX + d)
+        let n = mem[ix + d];
+        let f = r8.get(i8.F);
+        t.is(n, n1);
+        t.is(f, f1, `Result: ${f.toString(16)} Expected: ${f1.toString(16)}`);
+    }
+
+    doTest(7, 0x1, 0xf4, 0xf4, 0x00, 0x98);
+    doTest(4, 0x1, 0x41, 0x41, 0x01, 0x5d);
+});
+
+test('set_b_r(b, rIndex)', t => {
+    function doTest(b, r0, r1) {
+        const rIndex = i8.B;
+        r8.set(rIndex, r0);
+        instr.set_b_r(b, rIndex); // SET b, r
+        let r = r8.get(rIndex);
+        t.is(r, r1, `Result: ${r.toString(16)} Expected: ${r1.toString(16)}`);
+    }
+
+    doTest(3, 0x42, 0x4a);
 });
