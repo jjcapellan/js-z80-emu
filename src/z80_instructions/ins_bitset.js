18273d0eaf441d7635da42ef9b2950b5c16ac7c7
jjcapellan <soycape@hotmail.com>
2021-07-09 18:34:31 +0200

    Change: F5 and F3 flags behavior described in Undocumented Z80

--- src/z80_instructions/ins_bitset.js
+++ src/z80_instructions/ins_bitset.js
@@ -1,6 +1,8 @@
 /**
  * This file implements Z80 bit set, reset, and test instructions group
  * Info on page 242 of [manual](http://www.zilog.com/docs/z80/um0080.pdf)
+ * For BIT instructions flags see page 15 of 
+ * [The undocumented Z0 by Sean Young](http://www.z80.info/zip/z80-documented.pdf)
  * 
  * @author Juan Jose Capellan <soycape@hotmail.com>
  */
@@ -28,17 +30,42 @@ function bit_b_r(b, rIndex) {
         flags.get(fi.C),
         false,
         z,
-        (r & (1 << fi.F3)) != 0,
+        ((r & (1 << fi.F3)) != 0) && (b == 3),
         true,
-        (r & (1 << fi.F5)) != 0,
+        ((r & (1 << fi.F5)) != 0) && (b == 5),
         z,
         (b == 7) && (z == 0)
     );
     r8.set(i8.F, f);
-
 }
 
+/**
+* BIT b, (HL)
+* 
+* This instruction tests bit b in the memory location specified by the contents
+* of the HL register pair and sets the Z flag accordingly.
+* Clock: 12T
+*/
+function bit_b_ptrHL(b) {
+    const hl = r16.get(i16.HL);
+    const n = mem[hl];
+    const z = ((n & (1 << b)) == 0) ? true : false;
+    let f = createFlags(
+        flags.get(fi.C),
+        false,
+        z,
+        (n & (1 << fi.F3)) != 0,
+        true,
+        (n & (1 << fi.F5)) != 0,
+        z,
+        (b == 7) && (z == 0)
+    );
+    r8.set(i8.F, f);
+}
+    
+    
 module.exports = {
     bit_b_r,
+    bit_b_ptrHL,
     setCPU
 }
