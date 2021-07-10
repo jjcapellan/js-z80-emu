a6250c09706b369526badb471076695b3c707176
jjcapellan <soycape@hotmail.com>
2021-07-10 13:36:24 +0200

    Instruction: CALL PO, nn

--- src/z80_instructions/ins_call_return.js
+++ src/z80_instructions/ins_call_return.js
@@ -73,6 +73,16 @@ function call_c_nn(nn) {
         call_nn(nn);
 }
 
+/**
+* CALL PO, nn
+* 
+* Clock: 17T (condition true)    10T (condition false)
+*/
+function call_po_nn(nn) {
+    if (!flags.get(fi.PV))
+        call_nn(nn);
+}
+
 module.exports = {
     setCPU
 }
