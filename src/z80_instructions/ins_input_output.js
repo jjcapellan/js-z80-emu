2b4e34e39d73a687b9685af791ccd4e90e48790c
jjcapellan <soycape@hotmail.com>
2021-07-11 21:41:08 +0200

    Instruction: IN r, (C)

--- src/z80_instructions/ins_input_output.js
+++ src/z80_instructions/ins_input_output.js
@@ -23,5 +23,21 @@ function setCPU(data) {
 */
 function in_A_n(n) {
     const a = r8.get(i8.A);
-    r8.set(i8.A, ports[a*256 + n]);    
+    r8.set(i8.A, ports[a * 256 + n]);
+}
+
+/**
+* IN r, (C)
+* 
+* The contents of Register C are placed on the bottom half (A0 through A7) of the address
+* bus to select the I/O device at one of 256 (*1) possible ports. The contents of Register B are
+* placed on the top half (A8 through A15) of the address bus at this time. Then one byte
+* from the selected port is placed on the data bus and written to register r in the CPU.
+* Register r identifies any of the CPU registers. The flags are affected, checking the input data.
+* 1*) In fact 16bit port address is used (BC)
+* Clock: 12T
+*/
+function in_r_C(rIndex) {
+    const bc = r16.get(i16.BC);
+    r8.set(rIndex, ports[bc]);
 }
