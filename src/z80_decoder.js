b243f165734777c189a1577d988978e7d67429f3
jjcapellan <soycape@hotmail.com>
2021-07-13 18:33:28 +0200

    Decoded: 0xbb CP E

--- src/z80_decoder.js
+++ src/z80_decoder.js
@@ -901,7 +901,8 @@ function decode0xbX(byte) {
             ins_8bit_arithmetic.cp_r(i8.D);
             break;
 
-        case 0xbb:
+        case 0xbb: // CP E
+            ins_8bit_arithmetic.cp_r(i8.E);
             break;
 
         case 0xbc:
